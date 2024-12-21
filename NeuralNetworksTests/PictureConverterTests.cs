abe6526374dcc79958b2848dea3586073ad58e61
Borushko Dima <borushkoddima@mail.ru>
2024-12-21 15:11:42 +0300

    Реализовано преобразование изображения в черно-белый формат

---
+++ NeuralNetworksTests/PictureConverterTests.cs
@@ -0,0 +1,14 @@
+﻿namespace NeuralNetworks.Tests
+{
+    [TestClass()]
+    public class PictureConverterTests
+    {
+        [TestMethod()]
+        public void ConvertTest()
+        {
+            var converter = new PictureConverter();
+            var inputs = converter.Convert(@"C:\Users\borus\source\repos\NeuralNetworks\NeuralNetworksTests\Images\Parasitized.png");
+            converter.Save("d:\\image.png", inputs);
+        }
+    }
+}
